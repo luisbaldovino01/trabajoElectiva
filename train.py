bb6fd4363d4a5205ba1dfb23415a54824a41775a
luisbaldovino01 <125561890+luisbaldovino01@users.noreply.github.com>
2026-04-24 07:18:40 -0500

    Update train.py

--- train.py
+++ train.py
@@ -1,26 +1,22 @@
-import pandas as pd
-import pickle
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
-from sklearn.neighbors import KNeighborsRegressor
+def entrenar_modelo():
+    import pandas as pd
+    import pickle
+    from sklearn.model_selection import train_test_split
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.neighbors import KNeighborsRegressor
 
-data = pd.read_csv("dataset_ciclismo_fatiga.csv")
+    data = pd.read_csv("dataset_ciclismo_fatiga.csv")
 
-X = data[["frecuencia_cardiaca","potencia","cadencia","tiempo","temperatura","pendiente","velocidad"]]
-y = data["fatiga"]
+    X = data[["frecuencia_cardiaca","potencia","cadencia","tiempo","temperatura","pendiente","velocidad"]]
+    y = data["fatiga"]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
 
-# modelo final (puedes cambiar a KNN si quieres)
-model = KNeighborsRegressor(n_neighbors=5)
-model.fit(X_train, y_train)
+    model = KNeighborsRegressor(n_neighbors=5)
+    model.fit(X_train, y_train)
 
-# guardar scaler y modelo
-pickle.dump(model, open("modelo_casas.pkl", "wb"))
-pickle.dump(scaler, open("scaler.pkl", "wb"))
-
-print("Modelo entrenado y guardado")
+    pickle.dump(model, open("modelo_fatiga.pkl", "wb"))
+    pickle.dump(scaler, open("scaler.pkl", "wb"))
